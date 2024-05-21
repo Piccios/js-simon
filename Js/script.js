e8740f4bd6ddc90bccbcf33145ec2809643e571c
Piccio <lorenzo.hl.picchi@gmail.com>
2024-05-21 17:50:37 +0200

    issues with uniquenumbers function

--- Js/script.js
+++ Js/script.js
@@ -8,17 +8,14 @@ const playButton = document.querySelector('button#play');
 
 playButton.addEventListener('click', function () {
     generateNewGame(5); 
-    countDown(30);
+    countDown(5);
 });
 
-/**
- * 
- * @param {*} number 
- */
+// new game function
 
 function generateNewGame(number) {
     containerElement.innerHTML = '';
-    const uniqueNumbers = getUniqueNumbers(number, 1, 20);
+    const uniqueNumbers = getUniqueNumbers(5);
 
     for (let i = 0; i < number; i++) {
         const newDiv = document.createElement('article');
@@ -28,40 +25,61 @@ function generateNewGame(number) {
     }
 }
 
-function hideNumbers() {
-    const boxes = document.querySelectorAll('.box');
-    boxes.forEach(function(box) {
-        box.style.display = 'none';
-    })
+// function to get unique numbers
+
+function getUniqueNumbers(numberofCells) {
+    let numbers = [];
+    while (numbers.lenght < numberofCells) {
+        let randomNumber = Math.floor(Math.random() * (max - min + 1)) + min;
+        if (!numbers.includes(randomNumber)) {
+            numbers.push(randomNumber);
+        }
+    }
 }
 
 
-
-/**
- * 
- * @param {*} time 
- */
+// countdown timer that pops up the promps in which the user has to guess the numbers
 
 function countDown(time) {
-    let timer = ((time) * 1000);
-    setTimeout(function() {
-        alert('Time is up! Now guess the 5 numbers!')
-    }  ,timer)
+    let timer = time;
+    const interval = setInterval(() => {
+        if (timer > 0) {
+            console.log(`${timer} seconds remaining`);
+            timer--;
+        } else {
+            clearInterval(interval);
+            hideNumbers();
+            alert('Time is up! Now guess the 5 numbers!');
+            getUserGuess();
+        }
+    }, 1000);
 }
 
-// let userNumbers = [];
+// Function that hides the numbers
 
-// while (userNumbers.length < 5) {
-//     let num = parseInt(prompt(`What is your guess? (${userNumbers.length + 1}/5):`), 10);
-//     if (isNaN(num)) {
-//         alert("Please, insert a valid number:");
-//         continue;
-//     }
-//     if (userNumbers.includes(num)) {
-//         alert("This number has already been inserted, please try again:");
-//     } else {
-//         userNumbers.push(num);
-//     }
-// }
+function hideNumbers() {
+    const boxes = document.querySelectorAll('.box');
+    boxes.forEach(box => {
+        box.textContent = ''; 
+    });
+}
 
-// console.log("Your numbers are:", userNumbers);
+// Function that pops up prompts for user's guess
+
+function getUserGuess(){
+let userNumbers = [];
+
+while (userNumbers.length < 5) {
+    let num = parseInt(prompt(`What is your guess? (${userNumbers.length + 1}/5):`), 10);
+    if (isNaN(num)) {
+        alert("Please, insert a valid number:");
+        continue;
+    }
+    if (userNumbers.includes(num)) {
+        alert("This number has already been inserted, please try again:");
+    } else {
+        userNumbers.push(num);
+    }
+}
+console.log("Your numbers are:", userNumbers);
+}
