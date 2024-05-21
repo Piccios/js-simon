2546bce6c37c53f778eb5de54cab4c8dd21af655
Piccio <lorenzo.hl.picchi@gmail.com>
2024-05-21 17:05:07 +0200

    styled main, header and button

--- Js/script.js
+++ Js/script.js
@@ -15,23 +15,28 @@ function generateNewGame(number) {
     for (let i = 0; i < number; i++) {
         const newDiv = document.createElement('article');
         newDiv.classList.add('box');
-        
         containerElement.appendChild(newDiv);
     }
-    let userNumbers = [];
-
-while (userNumbers.length < 5) {
-    let num = parseInt(prompt(`What is your guess? (${userNumbers.length + 1}/5):`), 10);
-    if (isNaN(num)) {
-        alert("Please, insert a valid number:");
-        continue;
-    }
-    if (userNumbers.includes(num)) {
-        alert("This number has already been inserted, please try again:");
-    } else {
-        userNumbers.push(num);
-    }
+    
+    
 }
 
-console.log("Your numbers are:", userNumbers);
-}
+
+
+
+// let userNumbers = [];
+
+// while (userNumbers.length < 5) {
+//     let num = parseInt(prompt(`What is your guess? (${userNumbers.length + 1}/5):`), 10);
+//     if (isNaN(num)) {
+//         alert("Please, insert a valid number:");
+//         continue;
+//     }
+//     if (userNumbers.includes(num)) {
+//         alert("This number has already been inserted, please try again:");
+//     } else {
+//         userNumbers.push(num);
+//     }
+// }
+
+// console.log("Your numbers are:", userNumbers);
